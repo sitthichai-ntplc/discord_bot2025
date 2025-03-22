e8cf1b4481be8846efbd7fd695aef0380ac09f9c
sitthichai <125616543+sitthichai-ntplc@users.noreply.github.com>
2025-03-22 20:51:22 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,123 @@
+import os
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+from myserver import server_on
+
+bot = commands.Bot(camand_prefix='!',intents=discord.Intent.all())
+
+
+
+
+
+
+
+
+@bot.event
+async def on_ready():
+    print('Bot Online!') 
+    synced = await bot.tree.sync()
+    print(f'{len(synced)} command(s)')
+    
+    
+    
+    
+    
+    
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_channel(1352291855718678530)
+    text = f"Welcom to the server,{member.mention}!"
+    
+    emmbed = discord.Embed(title = 'Welcom to the server!',
+                           description = text,
+                           color = 0X66FFFF)
+    
+    await channel.send(text)
+    await channel.send(embed = emmbed)
+    await member.send(text)
+    
+    @bot.event
+    async def on_member_remove(member):
+        channel = bot.get_channel(1352291855718678530)
+        text = f"{member.name} has left the server!"
+        await channel.send(text)
+        
+@bot.event
+async def on_message(message):
+    mes = message.content # ดึงข้อความที่ถูกส่งมา
+    if mes == 'hello':
+        await message.channel.send("Hello It's me") # ส่งกลับไปที่ห้องนั่น
+
+    elif mes == 'hi bot':
+        await message.channel.send("Hello, " + str(message.author.name))
+
+    await bot.process_commands(message)
+    # ทำคำสั่ง event แล้วไปทำคำสั่ง bot command ต่อ
+
+
+
+
+# ///////////////////// Commands /////////////////////
+# กำหนดคำสั่งให้บอท
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"hello {ctx.author.name}!")
+
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
+
+
+# Slash Commands
+@bot.tree.command(name='hellobot', description='Replies with Hello')
+async def hellocommand(interaction):
+    await interaction.response.send_message("Hello It's me BOT DISCORD")
+
+
+@bot.tree.command(name='name')
+@app_commands.describe(name = "What's your name?")
+async def namecommand(interaction, name : str):
+    await interaction.response.send_message(f"Hello {name}")
+
+
+# Embeds
+
+@bot.tree.command(name='help', description='Bot Commands')
+async def helpcommand(interaction):
+    emmbed = discord.Embed(title='Help Me! - Bot Commands',
+                           description='Bot Commands',
+                           color=0x66FFFF,
+                           timestamp= discord.utils.utcnow())
+
+
+    # ใส่ข้อมูล
+    emmbed.add_field(name='/hello1', value='Hello Commmand', inline=True)
+    emmbed.add_field(name='/hello2', value='Hello Commmand', inline=True)
+    emmbed.add_field(name='/hello3', value='Hello Commmand', inline=False)
+
+    emmbed.set_author(name='Author', url='https://www.youtube.com/@maoloop01/channels', icon_url='https://yt3.googleusercontent.com/0qFq3tGT6LVyfLtZc-WCXcV9YyEFQ0M9U5W8qDe36j2xBTN34CJ20dZYQHmBz6aXASmttHI=s900-c-k-c0x00ffffff-no-rj')
+
+    # ใส่รูปเล็ก-ใหญ่
+    emmbed.set_thumbnail(url='https://yt3.googleusercontent.com/0qFq3tGT6LVyfLtZc-WCXcV9YyEFQ0M9U5W8qDe36j2xBTN34CJ20dZYQHmBz6aXASmttHI=s900-c-k-c0x00ffffff-no-rj')
+    emmbed.set_image(url='https://i.ytimg.com/vi/KZRa9DQzUpQ/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLCfWDgiBYjFJtrUasd5yxmQZJG_cg')
+
+    # Footer เนื้อหาส่วนท้าย
+    emmbed.set_footer(text='Footer', icon_url='https://yt3.googleusercontent.com/0qFq3tGT6LVyfLtZc-WCXcV9YyEFQ0M9U5W8qDe36j2xBTN34CJ20dZYQHmBz6aXASmttHI=s900-c-k-c0x00ffffff-no-rj')
+
+    await interaction.response.send_message(embed = emmbed)
+
+
+server_on()
+
+bot.run(os.getenv('TOKEN'))    
+    
+    
+    
+    
+    
+    
+    
